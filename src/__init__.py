37674daa007333728934f5aab793c29142b0e9af
Svetlana <207762751+lana-svetik@users.noreply.github.com>
2025-04-15 19:48:56 +0000

    Create __init__.py

---
+++ src/__init__.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Copyright (c) 2025 Svetlana Sibiryakova
+# /src/__init__.py
+# Initialisierungsmodul für das gpxFoto-Paket
+# Updated 2025-03-25
+
+"""
+gpxFoto - Ein einfaches Tool zur Erstellung von GPX-Tracks aus geogetaggten Dateien.
+
+Dieses Paket extrahiert GPS-Daten aus EXIF-Metadaten und
+erstellt GPX-Dateien, die in Kartenanwendungen verwendet werden können.
+"""
+
+__version__ = "1.0.0"
