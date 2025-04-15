572b0cca626fcc16e4f97f7a431cc14e594178b3
Svetlana <207762751+lana-svetik@users.noreply.github.com>
2025-04-15 20:03:39 +0000

    Create __main__.py

---
+++ src/__main__.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Copyright (c) 2025 Svetlana Sibiryakova
+# /src/__main__.py
+# Haupteinstiegspunkt für die direkte Paketausführung
+# Updated 2025-03-14
+
+"""
+Haupteinstiegspunkt für die direkte Paketausführung.
+"""
+
+from src.gpxfoto import main
+
+if __name__ == "__main__":
+    main()
